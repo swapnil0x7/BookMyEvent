8ffaaa24cfec9c7be8aeb46579fb6f3942d83e73
swapnil0x7 <swapnil0x7@gmail.com>
2024-10-10 13:33:01 +0530

    Created getMovies list and add Movie endpoints

--- server/controllers/movie.controllers.js
+++ server/controllers/movie.controllers.js
@@ -15,10 +15,27 @@ export const createMovie = async (req, res) => {
 
 export const getMovies = async (req, res) => {
   try {
-    const list = await Movie.find();
+    const type = req.query.type;
+    let queryFilter;
+
+    switch (type) {
+      case "UPCOMING":
+        queryFilter = { releaseDate: { $gte: "2024-10-11" } };
+        break;
+
+      case "LIVE":
+        queryFilter = { releaseDate: { $lte: "2024-10-10" } };
+        break;
+
+      default:
+        queryFilter = {};
+        break;
+    }
+
+    const list = await Movie.find(queryFilter);
     res.status(200).send({
       status: true,
-      message: "Movie created successfully!",
+      message: "Movie fetched successfully!",
       data: list,
     });
   } catch (error) {
